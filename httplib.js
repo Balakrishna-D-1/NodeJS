31986e32321aa9d8ee8141e770a056c9300aabf1
Balakrishna-D-1 <balakrishnad271@gmail.com>
2021-12-07 14:46:12 +0530

    updated

--- httplib.js
+++ httplib.js
@@ -4,19 +4,40 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type' : 'text/html'});
-
     if(req.url === '/home' || req.url === '/') {
-    let data = fs.readFileSync('Static/index.html');
-    res.write(data);
-}
-    else if(req.url === '/about') {
-        let data = fs.readFileSync('Static/about.html');
-    res.write(data);
-    }
-    else {
-        let data =  fs.readFileSync('Static/error.html');
+        let data = fs.readFileSync('Static/index.html');
         res.write(data);
     }
+        else if(req.url === '/about') {
+            let data = fs.readFileSync('Static/about.html');
+        res.write(data);
+        }
+      else  if(req.url === '/login' || req.url === '/') {
+            let data = fs.readFileSync('Static/login-page.html');
+            res.write(data);
+        }
+        else if(req.url === '/Register') {
+            let data = fs.readFileSync('Static/Register.html');
+        res.write(data);
+        }
+        else if(req.url === '/product') {
+            let data = fs.readFileSync('Static/product.html');
+        res.write(data);
+        }
+        else if(req.url === '/contact') {
+            let data = fs.readFileSync('Static/contact.html');
+        res.write(data);
+        }
+        else if(req.url === '/forgot') {
+            let data = fs.readFileSync('Static/forgot.html');
+        res.write(data);
+        }
+
+        else {
+            let data =  fs.readFileSync('Static/error.html');
+            res.write(data);
+        }
+    
 res.end();
 }).listen(8080, () => {
     console.log("server started port 8080")
